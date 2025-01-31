b20b3d261f335485a4ab33c2c5715af46e8ba397
Маздайщик <a.v.konovalov87@mail.ru>
2025-01-31 10:35:13 +0300

    Удалены неактуальные материалы

--- appendix/parser/README.md
+++
@@ -1,6 +0,0 @@
-Пример реализации парсера
-=========================
-
-* [parser-example.scm](parser-example.scm) 
-* [stream.scm](stream.scm) 
-* [trace.scm](trace.scm) 
--- appendix/parser/trace.scm
+++
@@ -1,9 +0,0 @@
-(define-syntax trace-ex
-  (syntax-rules ()
-    ((trace-ex expr)
-     (begin
-       (write 'expr)
-       (display " => ")
-       (let ((v expr))
-         (write v)
-         v)))))
